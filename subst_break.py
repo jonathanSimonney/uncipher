5a5f24e99de47e2a3b56a1a97c9f618eaccafa0d
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-05 22:54:37 +0100

    words set ready.

--- subst_break.py
+++ subst_break.py
@@ -1,7 +1,9 @@
 import argparse
+from collections import defaultdict
 
 
-parser = argparse.ArgumentParser(description='Tries to decode the encoded string by trying every combinaison in the latin alphabet range.')
+parser = argparse.ArgumentParser(description='Tries to decode the encoded string by trying every combinaison in the '
+                                             'latin alphabet range.')
 
 
 parser.add_argument('dictionary_path', type=str,
@@ -12,3 +14,15 @@ parser.add_argument('encoded_string', type=str,
 args = parser.parse_args()
 
 print(args.encoded_string, args.dictionary_path)
+
+words_set = set()
+try:
+    for line in open(args.dictionary_path, 'r'):
+        line = line.strip()
+        words_set.add(line)
+except FileNotFoundError:
+    print("There is no dictionary at " + args.dictionary_path + ". Please pass a valid file path for the program to "
+                                                                "function.")
+    exit()
+print("set filled")
+print(words_set)
